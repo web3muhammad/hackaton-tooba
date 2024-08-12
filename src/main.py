c307898db085583ff27f381a45012a7c41eec7da
web3muhammad <web3muhammad@icloud.com>
2024-08-12 08:32:19 +0300

    Добавил корс

--- src/main.py
+++ src/main.py
@@ -25,7 +25,10 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 @app.post("/bot")
